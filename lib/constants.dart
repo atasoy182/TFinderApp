15cf4201a7d562f02ae96d64dc09971ee48fd77c
mehmet berkay <atasoy182@gmail.com>
2021-04-30 21:18:10 +0300

    Default Tema rengi değiştirildi.

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,4 +11,10 @@ const pictureBgColor = Color.fromRGBO(48, 45, 44, 1);
 const topViewColor = Color(0xFFF5CEB8);
 const widgetBackgroundColor = Color.fromRGBO(240, 240, 240, 1);
 const textDisabledColor = Color.fromRGBO(126, 126, 126, 1);
+const turkuazRenk = Color.fromRGBO(65, 202, 198, 1);
 final kBoxShadowColor = Color(0xFFD3D3D3).withOpacity(.84);
+
+const turkuazDefault = Color.fromRGBO(65, 202, 198, 1);
+const turkuazWithOpacity = Color.fromRGBO(65, 202, 198, 0.8);
+const turkuazWithOpacity2 = Color.fromRGBO(65, 202, 198, 0.6);
+const turkuazWithOpacity3 = Color.fromRGBO(65, 202, 198, 0.4);
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -23,198 +23,199 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Colors.orange[900],
-            Colors.orange[800],
-            Colors.orange[400]
-          ])),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: 30,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Color.fromRGBO(65, 202, 198, 1),
+          Color.fromRGBO(65, 202, 198, 0.8),
+          Color.fromRGBO(65, 202, 198, 0.6),
+        ])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(
+                      1,
+                      Text(
+                        "Oturum Açın",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FadeAnimation(
+                      1.3,
+                      Text(
+                        "Hoş geldiniz",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    FadeAnimation(
-                        1,
-                        Text(
-                          "Oturum Açın",
-                          style: TextStyle(color: Colors.white, fontSize: 40),
-                        )),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    FadeAnimation(
-                        1.3,
-                        Text(
-                          "Hoş geldiniz",
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        )),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(60),
-                          topRight: Radius.circular(60))),
-                  child: SingleChildScrollView(
-                    child: Padding(
-                      padding: EdgeInsets.all(30),
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 30,
-                          ),
-                          FadeAnimation(
-                              1.4,
-                              Container(
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(10),
-                                    boxShadow: [
-                                      BoxShadow(
-                                          color:
-                                              Color.fromRGBO(225, 95, 27, .3),
-                                          blurRadius: 20,
-                                          offset: Offset(0, 10))
-                                    ]),
-                                child: Column(
-                                  children: <Widget>[
-                                    Container(
-                                      padding: EdgeInsets.all(10),
-                                      decoration: BoxDecoration(
-                                          border: Border(
-                                              bottom: BorderSide(
-                                                  color: Colors.grey[200]))),
-                                      child: TextField(
-                                        keyboardType:
-                                            TextInputType.emailAddress,
-                                        decoration: InputDecoration(
-                                            hintText: "Email",
-                                            hintStyle:
-                                                TextStyle(color: Colors.grey),
-                                            border: InputBorder.none),
-                                      ),
-                                    ),
-                                    Container(
-                                      padding: EdgeInsets.all(10),
-                                      decoration: BoxDecoration(
-                                          border: Border(
-                                              bottom: BorderSide(
-                                                  color: Colors.grey[200]))),
-                                      child: TextFormField(
-                                        decoration: InputDecoration(
-                                            hintText: "Şifre",
-                                            hintStyle:
-                                                TextStyle(color: Colors.grey),
-                                            border: InputBorder.none),
-                                        obscureText: _obscureText,
-                                        validator: (val) => val.length < 6
-                                            ? 'Şifre Çok Kısa'
-                                            : null,
-                                        onSaved: (val) => _password = val,
-                                      ),
-                                    ),
-                                    TextButton(
-                                        onPressed: _toggle,
-                                        child: new Text(_obscureText
-                                            ? "Şifreyi Göster"
-                                            : "Şifreyi Gizle"))
-                                  ],
-                                ),
-                              )),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          FadeAnimation(
-                              1.5,
-                              TextButton(
-                                  onPressed: _toggle,
-                                  child: Text(
-                                    "Şifrenizi mi unuttunuz ?",
-                                    style: TextStyle(color: Colors.grey),
-                                  ))),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          FadeAnimation(
-                            1.6,
-                            SizedBox(
-                              height: 50,
-                              width: 250,
-                              child: ElevatedButton(
-                                style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                            Colors.orange[900])),
-                                onPressed: () {},
-                                child: Text(
-                                  "Giriş Yap",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          FadeAnimation(
-                            1.6,
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 30,
+                        ),
+                        FadeAnimation(
+                            1.4,
                             Container(
-                              height: 50,
-                              width: 250,
-                              child: ElevatedButton(
-                                style: ButtonStyle(
-                                    backgroundColor:
-                                        MaterialStateProperty.all<Color>(
-                                            Colors.orange[900])),
-                                onPressed: () {},
-                                child: Row(
-                                  children: [
-                                    Spacer(),
-                                    Image.asset(
-                                      "assets/images/google.png",
-                                      width: 40,
-                                      height: 40,
-                                      fit: BoxFit.cover,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: turkuazWithOpacity3,
+                                        blurRadius: 20,
+                                        offset: Offset(0, 10))
+                                  ]),
+                              child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.grey[200]))),
+                                    child: TextField(
+                                      keyboardType: TextInputType.emailAddress,
+                                      decoration: InputDecoration(
+                                          hintText: "Email",
+                                          hintStyle:
+                                              TextStyle(color: Colors.grey),
+                                          border: InputBorder.none),
                                     ),
-                                    SizedBox(
-                                      width: 5,
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.all(10),
+                                    decoration: BoxDecoration(
+                                        border: Border(
+                                            bottom: BorderSide(
+                                                color: Colors.grey[200]))),
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                          hintText: "Şifre",
+                                          hintStyle:
+                                              TextStyle(color: Colors.grey),
+                                          border: InputBorder.none),
+                                      obscureText: _obscureText,
+                                      validator: (val) => val.length < 6
+                                          ? 'Şifre Çok Kısa'
+                                          : null,
+                                      onSaved: (val) => _password = val,
                                     ),
-                                    Text(
-                                      "Google ile giriş",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Spacer(),
-                                  ],
-                                ),
+                                  ),
+                                  TextButton(
+                                      onPressed: _toggle,
+                                      child: new Text(
+                                        _obscureText
+                                            ? "Şifreyi Göster"
+                                            : "Şifreyi Gizle",
+                                        style: TextStyle(
+                                          color: turkuazDefault,
+                                        ),
+                                      ))
+                                ],
+                              ),
+                            )),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        FadeAnimation(
+                            1.5,
+                            TextButton(
+                                onPressed: _toggle,
+                                child: Text(
+                                  "Şifrenizi mi unuttunuz ?",
+                                  style: TextStyle(color: Colors.grey),
+                                ))),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        FadeAnimation(
+                          1.6,
+                          SizedBox(
+                            height: 50,
+                            width: 250,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          turkuazDefault)),
+                              onPressed: () {},
+                              child: Text(
+                                "Giriş Yap",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
                               ),
                             ),
-                          )
-                        ],
-                      ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        FadeAnimation(
+                          1.6,
+                          Container(
+                            height: 50,
+                            width: 250,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          turkuazDefault)),
+                              onPressed: () {},
+                              child: Row(
+                                children: [
+                                  Spacer(),
+                                  Image.asset(
+                                    "assets/images/google.png",
+                                    width: 40,
+                                    height: 40,
+                                    fit: BoxFit.cover,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    "Google ile giriş",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Spacer(),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
